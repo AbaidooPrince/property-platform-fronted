00d39903379695dc183049a02d51fe112c315ef0
PrinceAbaidoo <70167148+PrinceAbaidoo@users.noreply.github.com>
2023-06-10 10:49:31 +0000

    refix image upload component

---
+++ components/forms/input/ImageUploadComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-card>
+            {{ props.src }}
+        </b-card>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+// import {ImageUploadProps} from './Input.types'
+
+export default defineComponent({
+    name: 'ImageUploadComponent'
+})
+</script>
+<script lang="ts" setup>
+const props = defineProps<{
+    src: string
+    state: true | false 
+}>()
+</script>
